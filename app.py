09c55c6578c8031eaa44eab63bc1dc8aa78c87a2
Benjamin Workman <bdworkma@gmail.com>
2024-11-04 08:32:53 -0500

    Seventh Commit

--- app.py
+++ app.py
@@ -2,6 +2,12 @@ import streamlit as st
 import scipy.stats
 import time
 
+if 'experiment_no' not in st.session_state:
+    st.session_state['experiment_no'] = 0
+
+if 'df_experiment_results' not in st.session_state:
+    st.session_state['df_experiment_results'] = pd.DataFrame(columns=['no', 'iterations', 'mean'])
+
 st.header('Tossing a Coin')
 
 chart = st.line_chart([0.5])
@@ -29,6 +35,20 @@ start_button = st.button('Run')
 
 if start_button:
     st.write(f'Running the experiment of {number_of_trials} trials.')
+    st.session_state['experiment_no'] += 1
     mean = toss_coin(number_of_trials)
+    st.session_state['df_experiment_results'] = pd.concat([
+        st.session_state['df_experiment_results'],
+        pd.DataFrame(data=[[st.session_state['experiment_no'],
+                            number_of_trials,
+                            mean]],
+                     columns=['no', 'iterations', 'mean'])
+        ],
+        axis=0)
+    st.session_state['df_experiment_results'] = \
+        st.session_state['df_experiment_results'].reset_index(drop=True)
+
+st.write(st.session_state['df_experiment_results'])
+
 
 
