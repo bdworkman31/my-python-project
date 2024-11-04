6fa16880fe49d6ebc70d455dd37978c5ba4bcd26
Benjamin Workman <bdworkma@gmail.com>
2024-11-04 08:11:49 -0500

    Sixth Commit

--- app.py
+++ app.py
@@ -29,7 +29,6 @@ start_button = st.button('Run')
 
 if start_button:
     st.write(f'Running the experiment of {number_of_trials} trials.')
-
-
+    mean = toss_coin(number_of_trials)
 
 st.write('It is not a functional application yet. Under construction.')
